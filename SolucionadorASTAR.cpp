b264ee39e53ad97519f6eea7f6b3596c35306d27
Steve Moon <sung.moon@ucr.ac.cr>
2021-09-24 10:19:03 -0600

    pseudocode for astar

--- SolucionadorASTAR.cpp
+++ SolucionadorASTAR.cpp
@@ -12,18 +12,37 @@ SolucionadorASTAR::SolucionadorASTAR(){
 }
 
 Solucion * SolucionadorASTAR::solucione( Problema * problema){
+        /*
+    pseudocode
+    costo = heuristica + profundidad
+    unexplored = new lista nodos a ser evaluado a traversar 
+    explored =new lista nodos ya traversados 
+    
+    loop
+        nodo actual = nodo en unexplored con costo mas bajo
+        pop nodo actual de unexplored
+        add nodo actual a lista explored
+
+        if nodo actual es estado meta
+            return; (termina)
+
+        while (i< cantidad de vecinos de nodo actual)
+            if vecino no es traversable || vecino se encuentra en lista explorado
+                i ++ (ir a proximo vecino)
+
+            if nueva ruta a vecino es mas corto o vecino != nodo en open
+                set costo de nodo vecino
+                nodo actual = padre de nodo vecino
+                if nodo vecino no está en lista unexplored
+                    unexplored.push(vecino)ñ
+    --end of loop
+
+    
+    */
     Estado* estado = problema->getEstadoInicial();
     profundidad = problema->heuristica(estado);
     bool resuelto = false;
-    /*
-    
 
-    Lista * pasos = new Lista();
-    Solucion * solucion = new Solucion(pasos);
-    
-    while(!resuelto){
-
-    }*/
 }
 
 Solucion * SolucionadorASTAR::solucioneRec(Problema * problema){
